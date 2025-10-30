363baa6fdf84c5791b196612d46b431660ca2f5a
Baldur Alexandur Skjoldborg Thomsen <bath@itu.dk>
2025-10-30 15:17:06 +0100

    simple text ui

--- Assets/Scripts/PlayerStuff/Player.cs
+++ Assets/Scripts/PlayerStuff/Player.cs
@@ -3,25 +3,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _health = 100;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    [SerializeField] UI _ui;
+    float _score = 0;
 
     public void TakeDamage(float damage)
     {
         _health -= damage;
-        if (_health < 0)
+        if (_health <= 0)
         {
-            Destroy (gameObject);
+            Destroy(gameObject);
         }
+        _ui.updateHealth(_health);
+    }
+
+    public void IncreaseScore(float scoreIncrease)
+    {
+        _score += scoreIncrease;
+        _ui.updateScore(_score);
     }
 }
