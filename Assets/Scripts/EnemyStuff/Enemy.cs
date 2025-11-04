ae193ed754cae9248eb41b1be31764049592497d
emzar <95346980+MilGaming@users.noreply.github.com>
2025-11-04 17:35:13 +0100

    enemy idle ranges done

--- Assets/Scripts/EnemyStuff/Enemy.cs
+++ Assets/Scripts/EnemyStuff/Enemy.cs
@@ -9,26 +9,36 @@ public class Enemy : MonoBehaviour
     private GameObject _player;
     private float _currentHealth;
 
+    public Vector3 HomePosition { get; private set; }
+    public float WanderRadius => _data.wanderRadius;
+    public Vector2 WanderWaitRange => _data.wanderWaitRange;
+
     private void Awake()
     {
         _player = GameObject.FindWithTag("Player");
         _currentHealth = _data.health;
         _agent.updateRotation = false;
         _agent.updateUpAxis = false;
+        HomePosition = transform.position;
     }
-
 
     public void TakeDamage(float damage)
     {
         _currentHealth -= damage;
-        if (_currentHealth <= 0)
-        {
-            Destroy(gameObject);
-        }
+        if (_currentHealth <= 0) Destroy(gameObject);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.cyan;
+
+        Vector3 center = Application.isPlaying ? HomePosition : transform.position;
+
+        Gizmos.DrawWireSphere(center, _data.wanderRadius);
     }
 
-    public NavMeshAgent GetAgent(){ return _agent; }
-    public IAttack GetAttack() { return _attack;}
+
+    public NavMeshAgent GetAgent() { return _agent; }
+    public IAttack GetAttack() { return _attack; }
     public GameObject GetPlayer() { return _player; }
     public float GetChaseRange() { return _data.chaseRange; }
     public float GetAttackRange() { return _data.attackRange; }
