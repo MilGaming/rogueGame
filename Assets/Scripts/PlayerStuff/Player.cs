2d32472aa7856a3d2b89fb03980031013528fc34
bath <bath@itu.dk>
2025-11-04 20:51:05 +0100

    small bug fixes

--- Assets/Scripts/PlayerStuff/Player.cs
+++ Assets/Scripts/PlayerStuff/Player.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float _health = 100;
-    [SerializeField] UI _ui;
+    UI _ui;
     float _score = 0;
     public event Action<GameObject /*killer*/> OnDied;
 
+    private void Start()
+    {
+        _ui = FindAnyObjectByType<UI>();
+    }
     public void TakeDamage(float damage, GameObject attacker)
     {
         _health -= damage;
