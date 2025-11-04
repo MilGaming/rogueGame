a64cb6bb47d379c2bc4750f28d284106171f067e
emzar <95346980+MilGaming@users.noreply.github.com>
2025-11-04 17:38:30 +0100

    fixed baldur shit code

--- Assets/Scripts/EnemyStuff/GetInRangeState.cs
+++ Assets/Scripts/EnemyStuff/GetInRangeState.cs
@@ -13,6 +13,8 @@ public class GetInRangeState : BaseState
 
     public override void Execute()
     {
+        if (_player == null || !_player.activeInHierarchy)
+            return;
         _agent.SetDestination(_player.transform.position);
         
 
@@ -24,6 +26,9 @@ public class GetInRangeState : BaseState
 
     public override BaseState GetNextState()
     {
+        if (_player == null || !_player.activeInHierarchy)
+            return new IdleState(_enemy);
+
         if (Vector3.Distance(_agent.transform.position, _player.transform.position) > _enemy.GetChaseRange())
         {
             return new IdleState(_enemy);  // Transition to chase if player is detected
